b5f7d43a6b317fc1199c06d46f42cd07cb3e4e72
Jesús Emilio <157801433+jesunixtux@users.noreply.github.com>
2026-08-13 00:26:04 -0400

    ATLAS: add penguin graphics-test CLI flag

--- Engine/Core/CommandLine.h
+++ Engine/Core/CommandLine.h
@@ -12,6 +12,7 @@ namespace Jevaing::Internal
         bool SelfTest = false;
         bool ShowRendererInfo = false;
         bool GraphicsTest = false;
+        bool PenguinGraphicsTest = false;
         bool HasFrameLimit = false;
         std::uint64_t FrameLimit = 0;
         std::string Renderer;
